fc46d6ba3fa4bedf6e445f6f97d09aca85b39f0f
Mec <mk01@gmail.com>
2025-07-15 11:23:59 +0800

    feat: 更新 demo 路由

---
+++ src/pages/demo/Image.vue
@@ -0,0 +1,24 @@
+<template>
+  <PageContainer :has-header="false" content-class="pt-16">
+    <div class="flex flex-col gap-16">
+      <LazyImage
+        v-for="item in 20"
+        :key="item"
+        height="400px"
+        width="400px"
+        :img-url="`https://picsum.photos/200/300?random=${item}`"
+        fit="cover"
+      />
+    </div>
+  </PageContainer>
+</template>
+
+<route lang="json5">
+  {
+    name: 'lazy-image',
+    meta: {
+      title: '图片懒加载',
+      i18n: 'menus.lazyImage'
+    },
+  }
+  </route>
---
+++ src/pages/demo/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <PageContainer :has-header="false" content-class="pt-16">
+    demo
+  </PageContainer>
+</template>
+
+<route lang="json5">
+  {
+    name: 'demo',
+    meta: {
+      title: 'demo',
+      i18n: 'menus.demo',
+    },
+  }
+</route>
