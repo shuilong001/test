eef3c49ebb5a52c229cde3615414b07eb1617781
mk <mk@gmail.com>
2025-08-06 11:54:14 +0800

    style: 移动端首页处理

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,7 +15,7 @@ onActivated(() => {
 
 <template>
   <PageContainer>
-    <div class="pb-100 flex flex-col md:px-16 md:pt-38">
+    <div class="pb-70 flex flex-col md:px-16 md:pt-38">
       <div class="h-408 w-full overflow-hidden">
         <div v-if="isDesktop">
           <img src="@/assets/images/home/pc-banner.png" alt="" class="h-full w-full md:cursor-pointer">
@@ -46,8 +46,8 @@ onActivated(() => {
           </template>
         </van-swipe>
       </div>
-      <div class="mt-16 px-10 flex flex-col gap-12">
-        <div class="flex-x-slide gap-11">
+      <div class="mt-16 flex flex-col gap-12">
+        <div class="px-10 flex-x-slide gap-11">
           <div class="flex-x-slide-item h-124 w-111">
             <img src="@/assets/images/demo/Frame 2087327677.png" alt="" class="h-full w-full">
           </div>
@@ -68,56 +68,134 @@ onActivated(() => {
           </div>
         </div>
         <div class="flex flex-col gap-7">
-          <div class="text-size-14 lh-19">
-            Recent Wins
+          <div class="px-10 flex items-center justify-between">
+            <div class="text-size-14 lh-19">
+              Recent Wins
+            </div>
+            <div class="text-size-10 lh-14 flex gap-1 items-center">
+              View All
+              <div class="i-custom:arrow-right h-8.52 w-8.52" />
+            </div>
           </div>
           <Vue3Marquee :duration="10">
-            <div class="flex flex-col gap-5">
+            <div class="mr-10 flex flex-col gap-5">
               <img src="@/assets/images/demo/Hot Chilli.png" class="h-91 w-68">
-              <div class="flex items-center">
-                <div>@User2314</div>
-              </div>
-              <div class="flex items-center">
-                <div>10,45,569</div>
+              <div class="flex flex-col gap-3 items-center">
+                <div class="flex items-center">
+                  <div class="i-custom:account-circle h-13 w-13" />
+                  <div class="text-size-10 lh-14">
+                    @User2314
+                  </div>
+                </div>
+                <div class="flex items-center">
+                  <div class="rounded-full bg-#074BD4 flex-center wh-10">
+                    <div class="i-custom:score h-6.3 w-7" />
+                  </div>
+                  <div class="text-size-8 lh-11">
+                    10,45,569
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="flex flex-col gap-5">
+            <div class="mr-10 flex flex-col gap-5">
               <img src="@/assets/images/demo/Hot Chilli (1).png" class="h-91 w-68">
-              <div class="flex items-center">
-                <div>@User2314</div>
-              </div>
-              <div class="flex items-center">
-                <div>10,45,569</div>
+              <div class="flex flex-col gap-3 items-center">
+                <div class="flex items-center">
+                  <div class="i-custom:account-circle h-13 w-13" />
+                  <div class="text-size-10 lh-14">
+                    @User2314
+                  </div>
+                </div>
+                <div class="flex items-center">
+                  <div class="rounded-full bg-#074BD4 flex-center wh-10">
+                    <div class="i-custom:score h-6.3 w-7" />
+                  </div>
+                  <div class="text-size-8 lh-11">
+                    10,45,569
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="flex flex-col gap-5">
+            <div class="mr-10 flex flex-col gap-5">
               <img src="@/assets/images/demo/Hot Chilli (2).png" class="h-91 w-68">
-              <div class="flex items-center">
-                <div>@User2314</div>
-              </div>
-              <div class="flex items-center">
-                <div>10,45,569</div>
+              <div class="flex flex-col gap-3 items-center">
+                <div class="flex items-center">
+                  <div class="i-custom:account-circle h-13 w-13" />
+                  <div class="text-size-10 lh-14">
+                    @User2314
+                  </div>
+                </div>
+                <div class="flex items-center">
+                  <div class="rounded-full bg-#074BD4 flex-center wh-10">
+                    <div class="i-custom:score h-6.3 w-7" />
+                  </div>
+                  <div class="text-size-8 lh-11">
+                    10,45,569
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="flex flex-col gap-5">
+            <div class="mr-10 flex flex-col gap-5">
               <img src="@/assets/images/demo/Hot Chilli (3).png" class="h-91 w-68">
-              <div class="flex items-center">
-                <div>@User2314</div>
-              </div>
-              <div class="flex items-center">
-                <div>10,45,569</div>
+              <div class="flex flex-col gap-3 items-center">
+                <div class="flex items-center">
+                  <div class="i-custom:account-circle h-13 w-13" />
+                  <div class="text-size-10 lh-14">
+                    @User2314
+                  </div>
+                </div>
+                <div class="flex items-center">
+                  <div class="rounded-full bg-#074BD4 flex-center wh-10">
+                    <div class="i-custom:score h-6.3 w-7" />
+                  </div>
+                  <div class="text-size-8 lh-11">
+                    10,45,569
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="flex flex-col gap-5">
+            <div class="mr-10 flex flex-col gap-5">
               <img src="@/assets/images/demo/Hot Chilli (2).png" class="h-91 w-68">
-              <div class="flex items-center">
-                <div>@User2314</div>
-              </div>
-              <div class="flex items-center">
-                <div>10,45,569</div>
+              <div class="flex flex-col gap-3 items-center">
+                <div class="flex items-center">
+                  <div class="i-custom:account-circle h-13 w-13" />
+                  <div class="text-size-10 lh-14">
+                    @User2314
+                  </div>
+                </div>
+                <div class="flex items-center">
+                  <div class="rounded-full bg-#074BD4 flex-center wh-10">
+                    <div class="i-custom:score h-6.3 w-7" />
+                  </div>
+                  <div class="text-size-8 lh-11">
+                    10,45,569
+                  </div>
+                </div>
               </div>
             </div>
           </Vue3Marquee>
+        </div>
+        <div v-for="item in 3" :key="item" class="flex flex-col gap-7">
+          <div class="px-10 flex items-center justify-between">
+            <div class="text-size-14 lh-19">
+              Exclusive games
+            </div>
+            <div class="text-size-10 lh-14 flex gap-1 items-center">
+              View All
+              <div class="i-custom:arrow-right h-8.52 w-8.52" />
+            </div>
+          </div>
+          <div class="px-10 flex-x-slide gap-11">
+            <div class="flex-x-slide-item h-148 w-111">
+              <img src="@/assets/images/demo/Go Rush.png" alt="" class="h-full w-full">
+            </div>
+            <div class="flex-x-slide-item h-148 w-111">
+              <img src="@/assets/images/demo/God Of Martial.png" alt="" class="h-full w-full">
+            </div>
+            <div class="flex-x-slide-item h-148 w-111">
+              <img src="@/assets/images/demo/Gold Rush.png" alt="" class="h-full w-full">
+            </div>
+          </div>
         </div>
       </div>
     </div>
